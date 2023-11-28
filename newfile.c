643719578e91968a9779a9088b17cb7ab9d6d8c7
the-octacat <148719118+the-octacat@users.noreply.github.com>
2023-11-28 12:52:03 +0300

    Capstone
    Simple bus reservation

--- newfile.c
+++ newfile.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 
 struct Employees {
-    char name[XYZ];
-    char password[*****];
-};
+    char name[50];
+    char password[50];
+} employee;
 
 struct Booking {
     int busNumber;
@@ -12,295 +12,84 @@ struct Booking {
     float payment;
     char bookingtime[10];
     char arrivaltime[10];
-};
+    char seatPreference[20]; // Added seat preference,back,front,near a window
+} book;
 
 struct Bus {
     int busNumber;
     int totalSeats;
-    char source[A];
-    char destination[B];
+    char source[50];
+    char destination[50];
     int availableSeats;
     int bookedSeats;
-};
+} customer;
 
-int main(void) {
-    struct Employees login;
-    struct addbus( );
-    struct mofifybus( );
-    struct deletebus( );
-    struct viewAllbuses( );
-    struct searchbuses( );
-    struct book seats( );
-    struct issueticket( );
-    struct cancelbooking( );
-    struct viewBookingHistory( );Writing a complete C code for a capstone project is beyond the scope of a single response. However, I can provide you with a basic structure to get you started. Please note that this is a simplified example, and you may need to expand and modify it based on your specific project requirements.
-
-```c
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// Define structures for Employee, Bus, Booking, and Booking History
-struct Employee {
-    char username[20];
-    char password[20];
-};
-
-struct Bus {
-    int busID;
-    char source[50];
-    char destination[50];
-    // Add other details as needed
-};
-
-struct Booking {
-    int bookingID;
-    int busID;
-    char customerName[50];
-    int seatNumber;
-    // Add other details as needed
-};
-
-struct BookingHistory {
-    int bookingID;
-    char customerName[50];
-    char source[50];
-    char destination[50];
-    // Add other details as needed
-};
-
-// Function prototypes
-void employeeLogin();
-void addBus();
-void modifyBus();
-void deleteBus();
-void viewAllBuses();
-void searchBuses();
-void bookSeat();
-void issueTicket();
-void cancelBooking();
-void viewBookingHistory();
-void saveData();
-void loadData();
-
-// Global arrays to store data
-struct Employee employees[10];
-struct Bus buses[50];
-struct Booking bookings[100];
-struct BookingHistory bookingHistory[100];
-int employeeCount = 0;
-int busCount = 0;
-int bookingCount = 0;
-int historyCount = 0;
+// Function prototype
+void chooseSeatPreference();
 
 int main() {
-    loadData();  // Load existing data from files
-
-    int choice;
-
-    do {
-        printf("\n----- Bus Reservation System Menu -----\n");
-        printf("1. Employee Login\n");
-        printf("2. Bus Information Management\n");
-        printf("3. Booking Management\n");
-        printf("4. Booking History\n");
-        printf("5. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                employeeLogin();
-                break;
-            case 2:
-                // Call functions for bus information management
-                break;
-            case 3:
-                // Call functions for booking management
-                break;
-            case 4:
-                // Call functions for booking history
-                break;
-            case 5:
-                saveData();  // Save data to files before exiting
-                printf("Exiting the program. Thank you!\n");
-                break;
-            default:
-                printf("Invalid choice. Please try again.\n");
-        }
-
-    } while (choice != 5);
-
-    return 0;
-}
-
-// Implement functions for bus information management, booking management, and booking history
-// ...
-
-// Implement functions for employee login, file handling, and other auxiliary functions
-// ...
-```
-
-This is a basic framework, and you need to fill in the details of each function according to your project requirements. Remember to handle data validation, file I/O operations, and any additional features you may want to include.
-    struct Booking book;
-    struct Bus customer;
-
     printf("Employees name: ");
-    fgets(employee.name, sizeof(employee.name), stdin);
+    gets(employee.name);
     printf("Employees password: ");
-    fgets(employee.password, sizeof(employee.password), stdin);
+    gets(employee.password);
 
     printf("Enter passenger name: ");
-    fgets(book.passengerName, sizeof(book.passengerName), stdin);
-    printf("Enter seat number booked: ");
-    scanf("%d", &book.seatNumber);
-
+    gets(book.passengerName);
     printf("Enter paid amount: ");
     scanf("%f", &book.payment);
+
     if (book.payment <= 1000) {
-        printf("NOT ENOUGH MONEY");
-        return 1; // Exit with an error code
+        printf("NOT ENOUGH MONEY\n");
+        return 1;  // Exiting with an error code
     } else {
-        printf("YOU ARE GOOD TO GO");
+        printf("YOU ARE GOOD TO GO\n");
     }
 
     printf("Enter time of booking: ");
-    scanf(" %9s", book.bookingtime);
+    getchar(); // consume the newline character left in the buffer
+    fgets(book.bookingtime, sizeof(book.bookingtime), stdin);
     printf("Enter time of arrival: ");
-    scanf(" %9s", book.arrivaltime);
+    fgets(book.arrivaltime, sizeof(book.arrivaltime), stdin);
 
     printf("Enter Bus number: ");
     scanf("%d", &customer.busNumber);
     printf("Enter total number of seats in the bus: ");
     scanf("%d", &customer.totalSeats);
 
+    // Seat booking
+    chooseSeatPreference();
+
     printf("Enter starting point or source: ");
-    scanf(" %49[^\n]", customer.source);
+    getchar(); // consume the newline character left in the buffer
+    fgets(customer.source, sizeof(customer.source), stdin);
     printf("Enter destination: ");
-    scanf(" %49[^\n]", customer.destination);
+    fgets(customer.destination, sizeof(customer.destination), stdin);
 
-    printf("Enter booked seats: Writing a complete C code for a capstone project is beyond the scope of a single response. However, I can provide you with a basic structure to get you started. Please note that this is a simplified example, and you may need to expand and modify it based on your specific project requirements.
-
-```c
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// Define structures for Employee, Bus, Booking, and Booking History
-struct Employee {
-    char username[20];
-    char password[20];
-};
-
-struct Bus {
-    int busID;
-    char source[50];
-    char destination[50];
-    // Add other details as needed
-};
-
-struct Booking {
-    int bookingID;
-    int busID;
-    char customerName[50];
-    int seatNumber;
-    // Add other details as needed
-};
-
-struct BookingHistory {
-    int bookingID;
-    char customerName[50];
-    char source[50];
-    char destination[50];
-    // Add other details as needed
-};
-
-// Function prototypes
-void employeeLogin();
-void addBus();
-void modifyBus();
-void deleteBus();
-void viewAllBuses();
-void searchBuses();
-void bookSeat();
-void issueTicket();
-void cancelBooking();
-void viewBookingHistory();
-void saveData();
-void loadData();
-
-// Global arrays to store data
-struct Employee employees[10];
-struct Bus buses[50];
-struct Booking bookings[100];
-struct BookingHistory bookingHistory[100];
-int employeeCount = 0;
-int busCount = 0;
-int bookingCount = 0;
-int historyCount = 0;
-
-int main() {
-    loadData();  // Load existing data from files
-
-    int choice;
-
-    do {
-        printf("\n----- Bus Reservation System Menu -----\n");
-        printf("1. Employee Login\n");
-        printf("2. Bus Information Management\n");
-        printf("3. Booking Management\n");
-        printf("4. Booking History\n");
-        printf("5. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                employeeLogin();
-                break;
-            case 2:
-                // Call functions for bus information management
-                break;
-            case 3:
-                // Call functions for booking management
-                break;
-            case 4:
-                // Call functions for booking history
-                break;
-            case 5:
-                saveData();  // Save data to files before exiting
-                printf("Exiting the program. Thank you!\n");
-                break;
-            default:
-                printf("Invalid choice. Please try again.\n");
-        }
-
-    } while (choice != 5);
-
-    return 0;
-}
-
-// Implement functions for bus information management, booking management, and booking history
-// ...
-
-// Implement functions for employee login, file handling, and other auxiliary functions
-// ...
-```
-
-This is a basic framework, and you need to fill in the details of each function according to your project requirements. Remember to handle data validation, file I/O operations, and any additional features you may want to include.");
+    printf("Enter booked seats: ");
     scanf("%d", &customer.bookedSeats);
 
+    // Calculate available seats
     customer.availableSeats = customer.totalSeats - customer.bookedSeats;
     printf("Available seats=%d\n", customer.availableSeats);
 
-    printf("*****BOOKING PAGE******\n");
-    printf("Employee doing the booking name = %s", employee.name);
-    printf("Clients name = %s", book.passengerName);
-    printf("Paid amount = %f\n", book.payment);
-    printf("Clients Booking time = %s\n", book.bookingtime);
-    printf("Clients Bus number = %d\n", customer.busNumber);
-    printf("Clients Seat Number = %d\n", book.seatNumber);
-    printf("Client is Travelling from: %s  To: %s\n", customer.source, customer.destination);
+    // Display booking details
+    printf("\n***** BOOKING PAGE ******\n");
+    printf("Employee doing the booking name=%s", employee.name);
+    printf("Client's name=%s", book.passengerName);
+    printf("Paid amount=%f", book.payment);
+    printf("Client's Booking time=%s", book.bookingtime);
+    printf("Client's Bus number=%d", customer.busNumber);
+    printf("Client's Seat Number=%d", book.seatNumber);
+    printf("Client's Seat Preference=%s", book.seatPreference);
+    printf("Client is Travelling from:%s To:%s\n", customer.source, customer.destination);
 
     return 0;
 }
+
+void chooseSeatPreference() {
+    printf("Choose your seat preference (Front/Back/Window): ");
+    fgets(book.seatPreference, sizeof(book.seatPreference), stdin);
+
+    // Implement logic to find a suitable seat based on the preference
+    // ...
+    }    
